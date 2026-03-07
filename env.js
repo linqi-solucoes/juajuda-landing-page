32437cf3760dbc8fbf2e1e7d6e48bab4adf1404e
Juliano Frascino <juliano@linqi.com.br>
2026-03-07 15:17:53 -0300

    fix: atualiza URL do Apps Script (novo deploy com permissões)

--- env.js
+++ env.js
@@ -1,4 +1,4 @@
 const ENV = {
   RECAPTCHA_SITE_KEY: "6LemB4MsAAAAACghS35_ynbNbFF2nmAI_QgEEiVx",
-  SHEETS_URL: "https://script.google.com/macros/s/AKfycbyaZKGR_RKviBchfwNhvME7YVHLJEYwhReys8agWqayDAPq-cTkpqGXU-x_64jY2tLU2Q/exec"
+  SHEETS_URL: "https://script.google.com/macros/s/AKfycbxeoGbn-lkXHUtfqRwzjy1km4X2LnFsIwY2cRQw94ZtB1pLykZbr1s17GFJ18pWgcSA/exec"
 };
